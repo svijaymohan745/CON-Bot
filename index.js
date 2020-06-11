fef778f96c71815babde588a3e1842810c90b680
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-11 16:26:30 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = 'NzIwMzcxNjcxMjQ4MjczNTE4.XuHqyg.oJEgPR0Hy8HY34EnlOIg_F4rhOA';
+
+client.login(token);
+
+client.on('ready', () => {
+    console.log('Welcome me into this nasty World');
+})
+
+client.on('guildMemberAdd', member =>{
+
+    const channel = member.guild.channels.cache.find(channel => channel.name === "👥people-incoming");
+    if(!channel) return;
+
+
+    channel.send(`Hey ${member}, welcome to CON! :tada::hugging: ! Head over to <#716551185431265331>  to assign yourself roles and get into the channels :D Good luck, Have fun`)
+});
+
+client.on('message', message =>{
+ 
+    }
+)
+
+
