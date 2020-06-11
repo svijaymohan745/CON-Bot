1a58aa998a4d7d3efa9e637784f4d6d0ca961cee
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-11 18:05:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 const token = process.env.token;
 
 client.login(token);
+
 
 client.on('ready', () => {
     console.log('Welcome me into this nasty World');
