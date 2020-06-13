4f66d90dddaef227cd3c1b5a020feae7dece9c15
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-13 19:33:41 +0530

    setInterval function modified
    Weekly Custom Messages

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ client.on('ready', () => {
 setInterval(() => {
   testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
 }, 604800000);
+setInterval(() => {
+  testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+}, 691200000);
+setInterval(() => {
+  testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+}, 777600000);
+setInterval(() => {
+  testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+}, 864000000);
+setInterval(() => {
+  testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
+}, 950400000);
+ 
 })
 client.on('guildMemberAdd', member => {
   const embed = {
