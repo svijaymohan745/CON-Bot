4bb15e2d562bd42ecdcc8819e828b5cee2c2335f
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-13 18:48:45 +0530

    Added setInterval function
    Message Channel after a set Interval

--- index.js
+++ index.js
@@ -6,9 +6,47 @@ client.login(token);
 
 
 client.on('ready', () => {
-    console.log('Welcome me into this nasty World');
-})
+  var testChannel = client.channels.cache.find(channel => channel.id === '716557043628245025');
+  console.log('Welcome me into this nasty World');
 
+setInterval(() => {
+  testChannel.send("Just dropping in Some Love");
+}, 5000);
+})
+client.on('guildMemberAdd', member => {
+  const embed = {
+    "title": "Welcome To Committee Of Noobs",
+    "description": "Hello Peeps, Welcome to the CON server!",
+    "color": 831018,
+    "thumbnail": {
+      "url": "https://i.imgur.com/mGJVLXz.png"
+    },
+    "image": {
+      "url": "https://i.imgur.com/Ckzaw3t.png"
+    },
+    "fields": [
+      {
+        "name": "💣",
+        "value": "Before diving into the different games you love in the server, get your Roles first by Going to <#716551185431265331>."
+      },
+      {
+        "name": "🎮",
+        "value": "New to Discord ?, check out <#717339670974824519> to see how its done. Feel free to ping any of the admins for any kind of stuff."
+      },
+      {
+        "name": "🤖",
+        "value": "Join a voice channel, talk about",
+        "inline": true
+      },
+      {
+        "name": "💪",
+        "value": "memes and ofc Go Pwn Some Noobs!",
+        "inline": true
+      }
+    ]
+  };
+ member.send({embed});
+},
 client.on('guildMemberAdd', member =>{
 
     const channel = member.guild.channels.cache.find(channel => channel.name === "👥people-incoming");
@@ -16,11 +54,9 @@ client.on('guildMemberAdd', member =>{
 
 
     channel.send(`Hey ${member}, welcome to CON! :tada::hugging: ! Head over to <#716551185431265331>  to assign yourself roles and get into the channels :D Good luck, Have fun`)
-});
+}));
 
 client.on('message', message =>{
  
     }
 )
-
-
