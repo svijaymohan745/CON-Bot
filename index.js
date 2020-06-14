a727d5171e71d39bf96b6bf47c583e56677d0113
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-15 00:19:07 +0530

    minor updates to bot activity

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ if (message.content == "_meme"){
 client.on('ready', () => {
   var testChannel = client.channels.cache.find(channel => channel.id === '716557043628245025');
   console.log('Welcome me into this nasty World');
-  client.user.setActivity('_help', { type: 'LISTENING'}).catch(console.error);
+  client.user.setActivity('your darkest secrets [_help]', { type: 'LISTENING'}).catch(console.error);
 
 setInterval(() => {
   testChannel.send("Hey there humans, make sure you spam those contracts till there is none left. <:hi:716321862661177404>");
