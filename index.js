a4be1e40bf471aa4a7ea68938d41f3df816fd2c3
svijaymohan745 <66779123+svijaymohan745@users.noreply.github.com>
2020-06-21 13:16:34 +0530

    Removed Coins and XP

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const botconfig = require("./botconfig.json");
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = require("./token.json");
+const token = process.env.token;
 const fs = require("fs");
 bot.commands = new Discord.Collection();
-let xp = require("./xp.json");
 let purple = botconfig.purple;
-let coins = require("./coins.json")
 const { MessageEmbed  } = require("discord.js");
 const randomPuppy = require("random-puppy");
 
@@ -31,84 +29,6 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.login(token);
 
-
-
-bot.on("message", async message => {
-
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: botconfig.prefix
-    };
-  }
-
-  if(!coins[message.author.id]){
-    coins[message.author.id] = {
-      coins: 0
-    };
-  }
-
-  let coinAmt = Math.floor(Math.random() * 15) + 1;
-  let baseAmt = Math.floor(Math.random() * 15) + 1;
-  console.log(`${coinAmt} ; ${baseAmt}`);
-
-  if(coinAmt === baseAmt){
-    coins[message.author.id] = {
-      coins: coins[message.author.id].coins + coinAmt
-    };
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if (err) console.log(err)
-  });
-  let coinEmbed = new Discord.MessageEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#0000FF")
-  .addField("<:CONcoin:719543153107009558>", `${coinAmt} coins added!`);
-
-  message.channel.send(coinEmbed).then(msg => {
-    msg.delete({ timeout: 10000 })
-  })
-  }
-
-  let xpAdd = Math.floor(Math.random() * 7) + 8;
-  console.log(xpAdd);
-
-  if(!xp[message.author.id]){
-    xp[message.author.id] = {
-      xp: 0,
-      level: 1
-    };
-  }
-
-
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp =  curxp + xpAdd;
-  if(nxtLvl <= xp[message.author.id].xp){
-    xp[message.author.id].level = curlvl + 1;
-    let lvlup = new Discord.MessageEmbed()
-    .setTitle("Level Up!")
-    .setColor(purple)
-    .addField("New Level", curlvl + 1);
-
-    message.channel.send(lvlup)
-  }
-  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
-  });
-
-  let prefix = prefixes[message.guild.id].prefixes;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-
-});
 
 
 bot.on('message', msg=>{
